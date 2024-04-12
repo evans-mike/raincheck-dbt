728da9ec883b079485ae3f77e43870dc1afdc8d5
Mike Evans <mike.evans@fastmail.com>
2024-04-12 16:11:22 +0000

    added jinja for pivot table

--- models/staging/stripe/stg_stripe__payments.sql
+++ models/staging/stripe/stg_stripe__payments.sql
@@ -1,24 +1,22 @@
-with 
+with
 
-source as (
+    source as (select * from {{ source("stripe", "payment") }}),
 
-    select * from {{ source('stripe', 'payment') }}
+    transformed as (
 
-),
+        select
 
-transformed as (
+            id as payment_id,
+            orderid as order_id,
+            paymentmethod as payment_method,
+            created as payment_created_at,
+            status as payment_status,
+            round(amount / 100.0, 2) as payment_amount,
+            created as created_at
 
-  select
+        from source
 
-    id as payment_id,
-    orderid as order_id,
-    created as payment_created_at,
-    status as payment_status,
-    round(amount / 100.0, 2) as payment_amount,
-    created as created_at
+    )
 
-  from source
-
-)
-
-select * from transformed
+select *
+from transformed
